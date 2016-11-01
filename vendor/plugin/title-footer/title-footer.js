93ade98b46481354b3331601adf994c80e322afc
Axel Hodler <derxor@gmail.com>
2016-11-01 20:30:42 +0100

    add blocklab logo

--- vendor/plugin/title-footer/title-footer.js
+++ vendor/plugin/title-footer/title-footer.js
@@ -76,7 +76,7 @@ title_footer.initialize=function(title,background)
 
 	// Initialize properties according to parameters
 
-	this.background=background || 'rgba(0,0,0,0.1)';
+	this.background=background || 'rgba(0,0,0,0)';
 	var title=title || '';
 	if (title!='')
 	{
@@ -110,6 +110,10 @@ title_footer.initialize=function(title,background)
 	var a_element=document.createElement('a');
 	a_element.setAttribute('href','#/0');
 	a_element.appendChild(document.createTextNode(this.title));
+	var logo = document.createElement('img');
+	logo.style="height: 25; float: left;";
+	logo.src = 'images/blocklab_logo.svg';
+	a_element.appendChild(logo);
 	title_footer_p.appendChild(a_element);
 	var div_class_reveal=document.querySelectorAll('.reveal')[0];
 	div_class_reveal.appendChild(title_footer);
